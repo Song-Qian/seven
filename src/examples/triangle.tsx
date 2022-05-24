8034a478ee20694f64d30ad593cc83ef5db4aeee
sq <onlylove1172559463@vip.qq.com>
2022-05-24 17:49:58 +0800

    modify triangle calc

--- src/examples/triangle.tsx
+++ src/examples/triangle.tsx
@@ -19,7 +19,9 @@ export default defineComponent<ComponentPropsOptions<Props>, any, any>({
         let canvas = ref<HTMLCanvasElement>();
         let descartes = reactive<{ x: number, y: number, gutter: number }>({ x: 0, y: 0, gutter: 20 });
 
-        let hypotenuseLengthArgs = reactive<{ a: Geometry.Point, b: Geometry.Point, output: number }>({ a: { x: 0, y: 0 }, b: { x: 0, y: 0 }, output: 0 })
+        let sideLengthArgs = reactive<{ a: Geometry.Point, b: Geometry.Point, output: number }>({ a: { x: 0, y: 0 }, b: { x: 0, y: 0 }, output: 0 })
+        let rightAngleHypotenuseArgs = reactive<{ a: number, b: number, output: number }>({ a: 0, b: 0, output: 0 });
+        let rightAngleSideArgs = reactive<{ type: 0 | 1, a: Geometry.Point, b: Geometry.Point, output: number }>({ type: 0, a: { x: 0, y: 0 }, b: { x: 0, y: 0 }, output: 0 });
         
         let drawTriangle = () => {
             let ctx = canvas.value?.getContext && canvas.value.getContext("2d");
@@ -30,12 +32,12 @@ export default defineComponent<ComponentPropsOptions<Props>, any, any>({
             if (type.value[0] === "HypotenuseLength") { 
                 let { x, y, gutter } = descartes;
                 let aPoint = {
-                    x: x + hypotenuseLengthArgs.a.x * gutter,
-                    y: y - hypotenuseLengthArgs.a.y * gutter
+                    x: x + sideLengthArgs.a.x * gutter,
+                    y: y - sideLengthArgs.a.y * gutter
                 }
                 let bPoint = {
-                    x: x + hypotenuseLengthArgs.b.x * gutter,
-                    y: y - hypotenuseLengthArgs.b.y * gutter
+                    x: x + sideLengthArgs.b.x * gutter,
+                    y: y - sideLengthArgs.b.y * gutter
                 }
                 ctx.beginPath();
                 ctx.fillStyle = "red";
@@ -58,16 +60,116 @@ export default defineComponent<ComponentPropsOptions<Props>, any, any>({
                 ctx.closePath();
                 let aleng = Point.Distance({ x, y }, aPoint);
                 let bleng = Point.Distance({ x, y }, bPoint);
-                let cleng = Triangle.HypotenuseLength(aleng, bleng);
+                let angle = Point.IncludedAngle(bPoint, { x, y }, aPoint);
+                let cleng = Triangle.SideLength(aleng, bleng, angle);
                 ctx.beginPath();
                 ctx.fillStyle = "blue";
                 ctx.strokeStyle = "blue";
                 ctx.moveTo(aPoint.x, aPoint.y);
                 ctx.lineTo(bPoint.x, bPoint.y);
                 ctx.stroke();
-                ctx.fillText(String(cleng), (aPoint.x + bPoint.x) / 2, (aPoint.y + bPoint.y) / 2);
+                ctx.fillText(String(cleng / gutter), (aPoint.x + bPoint.x) / 2, (aPoint.y + bPoint.y) / 2);
                 ctx.closePath();
-                hypotenuseLengthArgs.output = cleng;
+                sideLengthArgs.output = cleng / gutter;
+            }
+
+            if (type.value[0] === 'RightAngleHypotenuse') {
+                let { x, y, gutter } = descartes;
+                let aPoint = {
+                    x: x + rightAngleHypotenuseArgs.a * gutter,
+                    y: y
+                }
+                let bPoint = {
+                    x: x,
+                    y: y - rightAngleHypotenuseArgs.b * gutter
+                }
+                ctx.beginPath();
+                ctx.fillStyle = "red";
+                ctx.strokeStyle = "red";
+                ctx.lineCap = "round";
+                ctx.lineJoin = "round";
+                ctx.lineWidth = 2;
+                ctx.moveTo(x, y);
+                ctx.arc(x, y, 5, 0, 2 * Math.PI);
+                ctx.moveTo(aPoint.x, aPoint.y);
+                ctx.arc(aPoint.x, aPoint.y, 5, 0, 2 * Math.PI);
+                ctx.moveTo(bPoint.x, bPoint.y);
+                ctx.arc(bPoint.x, bPoint.y, 5, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.moveTo(x, y);
+                ctx.lineTo(aPoint.x, aPoint.y);
+                ctx.moveTo(x, y);
+                ctx.lineTo(bPoint.x, bPoint.y);
+                ctx.fillText("a边", (x + aPoint.x) / 2, (y + aPoint.y) / 2 + 10);
+                ctx.fillText("b边", (x + bPoint.x) / 2, (y + bPoint.y) / 2 + 10);
+                ctx.stroke();
+                ctx.closePath();
+                let output = Triangle.RightAngleHypotenuse(rightAngleHypotenuseArgs.a * gutter, rightAngleHypotenuseArgs.b * gutter);
+                ctx.beginPath();
+                ctx.fillStyle = "blue";
+                ctx.strokeStyle = "blue";
+                ctx.lineCap = "round";
+                ctx.lineJoin = "round";
+                ctx.moveTo(aPoint.x, aPoint.y);
+                ctx.lineTo(bPoint.x, bPoint.y);
+                ctx.fillText(String(output / gutter), (aPoint.x + bPoint.x) / 2, (aPoint.y + bPoint.y) / 2);
+                ctx.stroke();
+                ctx.closePath();
+                rightAngleHypotenuseArgs.output = output / gutter;
+            }
+
+            if (type.value[0] === "RightAngleSide") { 
+                let { x, y, gutter } = descartes;
+                let aPoint = {
+                    x: x + rightAngleSideArgs.a.x * gutter,
+                    y: y
+                }
+                let bPoint = {
+                    x: x,
+                    y: y - rightAngleSideArgs.b.y * gutter
+                }
+                ctx.beginPath();
+                ctx.fillStyle = "red";
+                ctx.strokeStyle = "red";
+                ctx.lineCap = "round";
+                ctx.lineJoin = "round";
+                ctx.lineWidth = 2;
+                ctx.moveTo(x, y);
+                ctx.arc(x, y, 5, 0, 2 * Math.PI);
+                ctx.moveTo(aPoint.x, aPoint.y);
+                ctx.arc(aPoint.x, aPoint.y, 5, 0, 2 * Math.PI);
+                ctx.moveTo(bPoint.x, bPoint.y);
+                ctx.arc(bPoint.x, bPoint.y, 5, 0, 2 * Math.PI);
+                ctx.fill();
+                if (rightAngleSideArgs.type === 0) {
+                    ctx.moveTo(x, y);
+                    ctx.lineTo(aPoint.x, aPoint.y);
+                }
+                if (rightAngleSideArgs.type === 1) {  
+                    ctx.moveTo(x, y);
+                    ctx.lineTo(bPoint.x, bPoint.y);
+                }
+                ctx.lineTo(aPoint.x, aPoint.y);
+                ctx.stroke();
+                ctx.closePath();
+                let hypotenuse = Point.Distance(aPoint, bPoint);
+                let a = rightAngleSideArgs.type === 0 ? Point.Distance({ x, y }, { x: aPoint.x, y }) : Point.Distance({ x, y }, { x, y: bPoint.y });
+                let output = Triangle.RightAngleSide(hypotenuse, a);
+                ctx.beginPath();
+                ctx.strokeStyle = "blue";
+                ctx.fillStyle = "blue";
+                ctx.moveTo(x, y);
+                if (rightAngleSideArgs.type === 0) {
+                    ctx.lineTo(bPoint.x, bPoint.y);
+                    ctx.fillText(String(output / gutter), (x + bPoint.x) / 2 - 10, (y + bPoint.y) / 2);
+                }
+                if (rightAngleSideArgs.type === 1) {  
+                    ctx.lineTo(aPoint.x, aPoint.y);
+                    ctx.fillText(String(output / gutter), (x + aPoint.x) / 2, (y + aPoint.y) / 2 + 10);
+                }
+                ctx.stroke();
+                ctx.closePath();
+                rightAngleSideArgs.output = output / gutter;
             }
         }
         
@@ -166,21 +268,53 @@ export default defineComponent<ComponentPropsOptions<Props>, any, any>({
                 <div class="drawer-args">
                     <acro-collapse v-model={[type.value, "active-key"]} accordion>
                         <acro-collapse-item header="三角斜边长度" key="HypotenuseLength">
-                            <acro-form model={hypotenuseLengthArgs} layout="vertical">
+                            <acro-form model={sideLengthArgs} layout="vertical">
                                 <acro-form-item label="A边点x坐标">
-                                    <acro-slider v-model={hypotenuseLengthArgs.a.x} min={-20} max={20} step={1} onChange={drawTriangle} />
+                                    <acro-slider v-model={sideLengthArgs.a.x} min={-20} max={20} step={1} onChange={drawTriangle} />
                                 </acro-form-item>
                                 <acro-form-item label="A边点y坐标">
-                                    <acro-slider v-model={hypotenuseLengthArgs.a.y} min={-20} max={20} step={1} onChange={drawTriangle} />
+                                    <acro-slider v-model={sideLengthArgs.a.y} min={-20} max={20} step={1} onChange={drawTriangle} />
                                 </acro-form-item>
                                 <acro-form-item label="B边点x坐标">
-                                    <acro-slider v-model={hypotenuseLengthArgs.b.x} min={-20} max={20} step={1} onChange={drawTriangle} />
+                                    <acro-slider v-model={sideLengthArgs.b.x} min={-20} max={20} step={1} onChange={drawTriangle} />
                                 </acro-form-item>
                                 <acro-form-item label="B边点y坐标">
-                                    <acro-slider v-model={hypotenuseLengthArgs.b.y} min={-20} max={20} step={1} onChange={drawTriangle} />
+                                    <acro-slider v-model={sideLengthArgs.b.y} min={-20} max={20} step={1} onChange={drawTriangle} />
                                 </acro-form-item>
                                 <acro-form-item label="输出结果">
-                                    <acro-textarea model-value={ JSON.stringify(hypotenuseLengthArgs.output) } readonly/>
+                                    <acro-textarea model-value={ JSON.stringify(sideLengthArgs.output) } readonly/>
+                                </acro-form-item>
+                            </acro-form>
+                        </acro-collapse-item>
+                        <acro-collapse-item header="直角三角形斜边长" key="RightAngleHypotenuse">
+                            <acro-form model={rightAngleHypotenuseArgs} layout="vertical">
+                                <acro-form-item label="A边">
+                                    <acro-slider v-model={rightAngleHypotenuseArgs.a} min={-20} max={20} step={1} onChange={drawTriangle} />
+                                </acro-form-item>
+                                <acro-form-item label="B边">
+                                    <acro-slider v-model={rightAngleHypotenuseArgs.b} min={-20} max={20} step={1} onChange={drawTriangle} />
+                                </acro-form-item>
+                                <acro-form-item label="输出结果">
+                                    <acro-textarea model-value={ JSON.stringify(rightAngleHypotenuseArgs.output) } readonly/>
+                                </acro-form-item>
+                            </acro-form>
+                        </acro-collapse-item>
+                        <acro-collapse-item header="直角三角形高或底边" key="RightAngleSide">
+                            <acro-form model={rightAngleSideArgs} layout="vertical">
+                                <acro-form-item label="通径方向">
+                                    <acro-radio-group  v-model={rightAngleSideArgs.type} type="button" size="small" onChange={drawTriangle}>
+                                        <acro-radio value={0}>高</acro-radio>
+                                        <acro-radio value={1}>底边</acro-radio>
+                                    </acro-radio-group>
+                                </acro-form-item>
+                                <acro-form-item label="斜边A点位置">
+                                    <acro-slider v-model={rightAngleSideArgs.a.x} min={-20} max={20} step={1} onChange={drawTriangle} />
+                                </acro-form-item>
+                                <acro-form-item label="斜边B点位置">
+                                    <acro-slider v-model={rightAngleSideArgs.b.y} min={-20} max={20} step={1} onChange={drawTriangle} />
+                                </acro-form-item>
+                                <acro-form-item label="输出结果">
+                                    <acro-textarea model-value={ JSON.stringify(rightAngleSideArgs.output) } readonly/>
                                 </acro-form-item>
                             </acro-form>
                         </acro-collapse-item>
